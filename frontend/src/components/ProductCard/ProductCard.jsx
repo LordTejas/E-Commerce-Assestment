10e66f00beab27f0ea8596bf4db7ba9fb9d095ab
LordTejas <tejas5225@gmail.com>
2023-06-02 15:09:16 +0530

    Fix: Added Rating Component

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -23,6 +23,23 @@ const ProductCard = ({
     );
   };
 
+  const Rating = ({ rating }) => {
+    const filledStars = Math.floor(rating); // Number of filled stars
+    const remainingStars = 5 - filledStars; // Number of remaining stars
+  
+    return (
+      <span>
+        {[...Array(filledStars)].map((_, index) => (
+          <img key={index} src="star.png" alt="filled star" height='24px' width='24px' />
+        ))}
+        {[...Array(remainingStars)].map((_, index) => (
+          <img key={index + filledStars} src="unstar.png" alt="unfilled star" height='24px' width='24px' />
+        ))}
+      </span>
+    );
+  }
+  
+
   return (
     <section key={productId} className="product-card">
       <div className="product-images">
@@ -38,7 +55,10 @@ const ProductCard = ({
       </div>
       <div className="product-actions-area">
         <div className="product-header">
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
           <span className="product-title">{name}</span>
+          <Rating rating={rating} />
+          </div>
           <span className="product-subtitle">{brandName}</span>
           <span className="product-desc">{description.substring(0, 42)}</span>
         </div>
